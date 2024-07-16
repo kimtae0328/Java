ad1f3821aa266f6fe7f0a5838df5aa7be2a47d01
김태형 <kimtae0328@gmail.com>
2024-07-16 16:27:21 +0900

    240716

---
+++ src/com/human/A_App.java
@@ -0,0 +1,16 @@
+package com.human;
+
+import com.human.a_variable.VariableType;
+
+public class A_App {
+    public static void main(String[] args) {
+        VariableType v = new VariableType();
+        v.type1();
+        System.out.println("===========================");
+        v.type2();
+        v.type3();
+        v.type4();
+        v.type5();
+        v.type6();
+    }
+}
---
+++ src/com/human/Application.java
@@ -0,0 +1,13 @@
+package com.human;
+
+import com.human.a_variable.Variable;
+
+public class Application {
+    public static void main(String[] args) {
+        Variable v = new Variable();
+        v.var1();
+        v.var2();
+        System.out.println();
+        v.gugudan();
+    }
+}
